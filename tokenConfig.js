d597eaa08bffb46e18992f0ad80ad45a7b6b1b9b
feigebaobei <18515195415@163.com>
2020-08-05 15:26:55 +0800

    tokenSDKServer init

---
+++ tokenConfig.js
@@ -0,0 +1,5 @@
+const tokenSDKServer = require('token-sdk-server')
+const {receiveMessage} = require('./lib/utils.js')
+const fs = require('fs')
+let {didttm, idpwd} = require('./importConfig.js')
+tokenSDKServer.init(didttm, idpwd, receiveMessage)
